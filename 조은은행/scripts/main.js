b4b58d77e86e8ff6fc92b0625d2e128fcafc413c
ggang89 <ggang89@gmail.com>
2024-03-15 17:33:15 +0900

    slide 좌우로 이동하는거 css + 제이쿼리로 구현

--- 조은은행/scripts/main.js
+++ 조은은행/scripts/main.js
@@ -11,3 +11,18 @@ subMenu.stop().slideDown();
   header.removeClass('active');
 subMenu.stop().slideUp();
 });
+
+// 슬라이드
+let slideContainer = $('.container');
+let slide = $('.slide');
+let slideCount = slide.length;
+let currentIdx = 0;
+
+function autoSlide(){
+  setInterval(function(){
+    let nextIdx = (currentIdx+1)%slideCount;
+    slideContainer.animate({marginLeft:-1200*nextIdx+'px'});
+    currentIdx = nextIdx
+  },3000)
+}
+autoSlide();
