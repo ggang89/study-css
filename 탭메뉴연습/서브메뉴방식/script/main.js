8664e882290ad4ba50c1ec7aa71a01ac4a8726bc
ggang89 <ggang89@gmail.com>
2024-03-19 01:28:13 +0900

    탑메뉴-서브메뉴방식으로 연습

--- 탭메뉴연습/서브메뉴방식/script/main.js
+++ 탭메뉴연습/서브메뉴방식/script/main.js
@@ -2,6 +2,6 @@ let tabMenu=$(".tabmenu>li");
 
 tabMenu.click(function(e){
   e.preventDefault();
-  tabMenu.removeClass('.active');
+  tabMenu.removeClass('active');
   $(this).addClass('active');
 });
